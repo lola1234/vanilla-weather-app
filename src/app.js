56016927bcd7087aad0f0976d363245080d892b0
lola1234 <101635306+lola1234@users.noreply.github.com>
2022-04-27 06:32:40 -0400

    Updated search engine, added relevant js search functions

--- src/app.js
+++ src/app.js
@@ -44,10 +44,17 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "79e53168a6395fac841976d6b3e55fe3";
+function search(city) {
+  let apiKey = "79e53168a6395fac841976d6b3e55fe3";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-let city = "New York";
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
 
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
-axios.get(apiUrl).then(displayTemperature);
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
